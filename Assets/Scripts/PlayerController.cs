3c831c71369d014d4163dc886462ef08533ace69
Antoine_2008 <151564128+antoine-2008@users.noreply.github.com>
2024-07-01 17:42:12 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 15f;
+
+    [SerializeField]
+    private float mouseSensitivityX = 15f;
+
+    [SerializeField]
+    private float mouseSensitivityY = 15f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        // Calculer la vélocité (vitesse) du mouvement du joueur
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMov;
+        Vector3 moveVertical = transform.forward * zMov;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        motor.Move(velocity);
+
+        // Calcule la rotation du joueur en un Vector3
+        float yRot = Input.GetAxisRaw("Mouse X");
+        float xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 rotation = new Vector3(0, yRot, 0) * mouseSensitivityX;
+
+        motor.Rotate(rotation);
+
+        Vector3 cameraRotation = new Vector3(xRot, 0, 0) * mouseSensitivityY;
+
+        motor.RotateCamera(cameraRotation);
+    }
+}
