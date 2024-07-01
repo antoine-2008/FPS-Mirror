71a34e4b9e831a666f35f79c6565559c326fcf33
Antoine_2008 <151564128+antoine-2008@users.noreply.github.com>
2024-07-01 18:37:02 +0200

    Dev 1.1
    Mise en place du multi joueur

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentToDisable;
+
+    Camera sceneCamera;
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+
+            // On va boucler sur les différents cmposants renseignés et les désactiver si ce joueur n'est pas le notre
+            for (int i = 0; i < componentToDisable.Length; i++)
+            {
+                componentToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if(sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
